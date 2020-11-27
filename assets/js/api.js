d429a4f19a524ca49eb02f3940f57aaae0ab9469
vpvp <vktrpnzrv@gmail.com>
2020-11-27 22:54:11 +0300

    Добавлена функция получения коллекции из БД

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,2 +1,19 @@
 // Функций для связывания проекта с back-end частью
-export default { }
+export default {
+    async getCollection(db, project_id, collection) {
+        let documents = []
+        const ref = await db
+            .firestore()
+            .collection('projects')
+            .doc(project_id)
+            .collection(collection)
+            .get()
+        ref.forEach((doc) => {
+            documents.push({
+                id: doc.id,
+                ...doc.data(),
+            })
+        })
+        return documents
+    },
+}
