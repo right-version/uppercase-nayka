e8ed4eff359646bd419d7c9c5ea66c6050286859
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2020-11-28 00:45:13 +0300

    Добавлено создание проектов

--- pages/create.vue
+++ pages/create.vue
@@ -4,42 +4,43 @@
       v-icon(large color="#212120") mdi-bookmark-plus-outline
       | Создание проекта
     
-    v-col(no-gutters)
-      .field-text.mt-5 Автор - {{ user.name }}
-      .field-text.mt-5 Дата и время - {{ form.date | date }}
-      v-text-field.mt-5(
-        label="Название проекта"
-        v-model="form.title"
-        :rules="nameRules"
-        filled
-      )
-      v-textarea(
-        v-model="form.description"
-        label="Описание"
-        filled
-        :rules="nameRules"
-      )
-      .field-text Организация
-      multiselect(
-        v-model="form.organisation"
-        :options="organisations"
-        :preserve-search="true"
-        placeholder="Выберите организацию"
-      )
-      .field-text.mt-5 Области исследования
-      multiselect(
-        v-model="form.areas"
-        :options="areas"
-        :multiple="true"
-        :close-on-select="false"
-        :clear-on-select="false"
-        :preserve-search="true"
-        placeholder="Выберите области исследования"
-      )
-      v-row.mt-5(no-gutters)
-        v-spacer
-        v-btn.default.mr-2(rounded @click.prevent="$router.go(-1)") Отмена
-        v-btn.danger(rounded @click.prevent="saveProject") Сохранить
+    v-form(v-model="valid" ref="form")
+      v-col(no-gutters)
+        .field-text.mt-5 Автор - {{ user.name }}
+        .field-text.mt-5 Дата и время - {{ form.date | date }}
+        v-text-field.mt-5(
+          label="Название проекта"
+          v-model="form.title"
+          :rules="nameRules"
+          filled
+        )
+        v-textarea(
+          v-model="form.description"
+          label="Описание"
+          filled
+          :rules="nameRules"
+        )
+        .field-text Организация
+        multiselect(
+          v-model="form.organisation"
+          :options="organisations"
+          :preserve-search="true"
+          placeholder="Выберите организацию"
+        )
+        .field-text.mt-5 Области исследования
+        multiselect(
+          v-model="form.areas"
+          :options="areas"
+          :multiple="true"
+          :close-on-select="false"
+          :clear-on-select="false"
+          :preserve-search="true"
+          placeholder="Выберите области исследования"
+        )
+        v-row.mt-5(no-gutters)
+          v-spacer
+          v-btn.default.mr-2(rounded @click.prevent="$router.go(-1)") Отмена
+          v-btn.danger(rounded @click.prevent="saveProject") Сохранить
 
 </template>
 
@@ -64,10 +65,23 @@ export default {
       areas: [],
     },
     nameRules: [(v) => !!v || 'Name is required'],
+    valid: true,
   }),
   computed: {
     user() {
       return this.$store.state.user
+    },
+  },
+  methods: {
+    async saveProject() {
+      this.$refs.form.validate()
+      if (!this.valid) return
+
+      const formData = { ...this.form, author: this.user }
+      const response = await api.addProject(this.$firebase, formData)
+      if (response.id) {
+        this.$router.push(`/project/${response.id}`)
+      }
     },
   },
 }
