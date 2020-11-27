0b5396b4bb2054ee0eb2b61ecb6aa12ed2026c38
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2020-11-28 02:51:20 +0300

    Создана страничка пользователя

---
+++ pages/user.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  .user-page
+    h1 {{ user.name }}
+    v-avatar.mt-5(
+      color="primary"
+      size="280"
+      tile
+    )
+      img(:src="user.image")
+</template>
+
+<script>
+export default {
+  computed: {
+    user() {
+      return this.$store.state.user
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.user-page {
+}
+</style>
