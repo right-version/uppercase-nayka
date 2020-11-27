a1c7b80270686d8678b73861c870cefc30545475
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2020-11-27 23:12:58 +0300

    Подключены проекты

--- pages/index.vue
+++ pages/index.vue
@@ -6,33 +6,16 @@
 </template>
 
 <script>
+import api from '~/assets/js/api'
 import ProjectCard from '~/components/Cards/ProjectCard.vue'
 export default {
   components: {
     ProjectCard,
   },
-  data: () => ({
-    projects: [
-      {
-        id: 'ldsafasfdsfld',
-        areas: ['Математика', 'Компьютерные науки', 'Математика'],
-        author: {
-          id: 'dadas',
-          name: 'Вим Олегович',
-          image: 'https://placekitten.com/g/200/200',
-        },
-        description:
-          'Сделать клевый сервис, что бы победить в хакатоне! Сделать клевый сервис, что бы ервис, что бы победить в хакатоне! Сделать клевый сервис, что бы победить в хакатоне!',
-        organisation: {
-          title: 'РИНХ',
-          id: '3132',
-          logo:
-            'https://hsto.org/getpro/moikrug/uploads/company/554/163/612/logo/medium_9e4f0044d4c42bf95c31e61e2049ef4a.png',
-        },
-        title: 'Разработка сервиса Наука',
-      },
-    ],
-  }),
+  async asyncData({ $firebase }) {
+    const projects = await api.getAllProjects($firebase)
+    return { projects }
+  },
 }
 </script>
 
