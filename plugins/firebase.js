2807e5967904aa3c9f1661755c0a41275ec9c91b
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2020-11-27 22:56:33 +0300

    Добавлен конфиг firebase

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/firestore'
 
 export default ({ env, store }, inject) => {
   const firebaseConfig = {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: ''
+    apiKey: process.env.FIREBASE,
+    authDomain: 'nauka-af6ed.firebaseapp.com',
+    databaseURL: 'https://nauka-af6ed.firebaseio.com',
+    projectId: 'nauka-af6ed',
+    storageBucket: 'nauka-af6ed.appspot.com',
+    messagingSenderId: '772950995216',
+    appId: '1:772950995216:web:5f9485ac9da664f811f595',
+    measurementId: 'G-ZDYJ8KS105',
   }
 
   if (!firebase.apps.length) {
