166ac6d811a5e6ec850e7c318bcfc95816a6d4ff
vpvp <vktrpnzrv@gmail.com>
2020-11-27 22:57:33 +0300

    Добавлены функции получения информации о проектах из БД

--- assets/js/api.js
+++ assets/js/api.js
@@ -16,4 +16,31 @@ export default {
         })
         return documents
     },
+
+    async getProjectInfo(db, project_id) {
+        const ref = await db
+            .firestore()
+            .collection('projects')
+            .doc(project_id)
+            .get()
+        return {
+            id: ref.id,
+            ...ref.data(),
+        }
+    },
+
+    async getAllProjects(db) {
+        let projects = []
+        const ref = await db
+            .firestore()
+            .collection('projects')
+            .get()
+        ref.forEach((pr) => {
+            projects.push({
+                id: pr.id,
+                ...pr.data(),
+            })
+        })
+        return projects
+    },
 }
