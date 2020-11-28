4ddd4c8333de21064d4a3517209cf4d6e49e5dbc
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2020-11-28 15:14:59 +0300

    fix: api

--- assets/js/api.js
+++ assets/js/api.js
@@ -17,6 +17,103 @@ export default {
     return documents
   },
 
+  async addProject(db, data) {
+    const ref = await db
+      .firestore()
+      .collection('projects')
+      .add({ ...data })
+    return ref
+  },
+
+  async getProjectInfo(db, project_id) {
+    const ref = await db
+      .firestore()
+      .collection('projects')
+      .doc(project_id)
+      .get()
+    return {
+      id: ref.id,
+      ...ref.data(),
+    }
+  },
+
+  async getAllProjects(db) {
+    let projects = []
+    const ref = await db.firestore().collection('projects').get()
+    ref.forEach((pr) => {
+      projects.push({
+        id: pr.id,
+        ...pr.data(),
+      })
+    })
+    return projects
+  },
+
+  async addDocument(db, project_id, collection, data) {
+    const ref = await db
+      .firestore()
+      .collection('projects')
+      .doc(project_id)
+      .collection(collection)
+      .add({ ...data, type: collection })
+    return ref
+  },
+
+  async deleteDocument(db, project_id, collection, document_id) {
+    const ref = await db
+      .firestore()
+      .collection('projects')
+      .doc(project_id)
+      .collection(collection)
+      .doc(document_id)
+      .delete()
+    return ref
+  },
+
+  async patchDocument(db, project_id, collection, document_id, data) {
+    const ref = await db
+      .firestore()
+      .collection('projects')
+      .doc(project_id)
+      .collection(collection)
+      .doc(document_id)
+      .set({
+        ...data,
+      })
+    return ref
+  },
+
+  async getDocument(db, project_id, collection, document_id) {
+    const ref = await db
+      .firestore()
+      .collection('projects')
+      .doc(project_id)
+      .collection(collection)
+      .doc(document_id)
+      .get()
+    return {
+      id: ref.id,
+      ...ref.data(),
+    }
+  },
+
+  async getCollection(db, project_id, collection) {
+    let documents = []
+    const ref = await db
+      .firestore()
+      .collection('projects')
+      .doc(project_id)
+      .collection(collection)
+      .get()
+    ref.forEach((doc) => {
+      documents.push({
+        id: doc.id,
+        ...doc.data(),
+      })
+    })
+    return documents
+  },
+
   async addDocument(db, project_id, collection, data) {
     const ref = await db
       .firestore()
